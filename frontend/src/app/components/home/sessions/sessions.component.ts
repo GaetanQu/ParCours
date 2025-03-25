e4ccd2b3a3452e792f3c669de0926e404e7eec0e
GaetanQu <gaetean.quenouille.gq@gmail.com>
2025-03-25 18:36:25 +0100

    ✨ feat: Implement session listing in sessions component and integrate session service for data retrieval

--- frontend/src/app/components/home/sessions/sessions.component.ts
+++ frontend/src/app/components/home/sessions/sessions.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SessionService } from '../../../service/session.service';
+import { SessionDTO } from '../../../model/session.dto';
+import { LoggerService } from '../../../service/logger.service';
 
 @Component({
   selector: 'app-sessions',
@@ -7,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './sessions.component.css'
 })
 export class SessionsComponent {
+  protected sessions:SessionDTO[] = [] as SessionDTO[]
 
+  constructor(
+    private sessionService: SessionService,
+    private loggerService: LoggerService
+  ) { }
+
+  ngOnInit() {
+    this.sessionService.getAllSessions().subscribe({
+      next: sessionsGot => {
+      this.sessions = sessionsGot;
+      },
+      complete: () => {
+        this.loggerService.info("Sessions successfully obtained")
+      }
+    });
+  }
 }
