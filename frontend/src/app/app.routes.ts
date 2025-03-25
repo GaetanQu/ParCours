e39e64b90f3fe178e08a00e9758de174ddf4ece6
GaetanQu <gaetean.quenouille.gq@gmail.com>
2025-03-25 18:07:50 +0100

    ✨ feat: Add dashboard, sessions, and homeworks components with routing integration and update header for improved navigation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './guard/auth.guard';
+import { SessionsComponent } from './components/home/sessions/sessions.component';
+import { DashboardComponent } from './components/home/dashboard/dashboard.component';
+import { HomeworksComponent } from './components/home/homeworks/homeworks.component';
 
 export const routes: Routes = [
     {   path: 'login',
@@ -12,6 +15,18 @@ export const routes: Routes = [
         canActivate: [authGuard],
 
         children: [
+            {
+                path: '',
+                component: DashboardComponent,
+            },
+            {
+                path: 'sessions',
+                component: SessionsComponent,
+            },
+            {
+                path: 'homeworks',
+                component: HomeworksComponent,
+            }
         ],
     },
 ];
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../partials/header/header.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [
+    RouterModule,
+    HeaderComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
