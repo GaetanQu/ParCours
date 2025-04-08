db7e2fe43213a0ed9ab8fa5b9977826ddf3e936e
GaetanQu <gaetean.quenouille.gq@gmail.com>
2025-04-08 20:15:55 +0200

    ✨ chore: Remove unused import statements from SecurityConfig, SessionController, Homework, and Session classes

--- parcours/src/main/java/fr/gaetanquenouille/parcours/model/Homework.java
+++ parcours/src/main/java/fr/gaetanquenouille/parcours/model/Homework.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Entity
